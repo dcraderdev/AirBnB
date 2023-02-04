182c6b117471fe49e5c3aac95b1666cb2df1ed18
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-04 13:35:37 -0800

    Add an API router
    Add an API router

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -10,6 +10,15 @@ router.get('/test', function(req, res) {
 });
 
 
+// fetch('/api/test', {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//   },
+//   body: JSON.stringify({ hello: 'world' })
+// }).then(res => res.json()).then(data => console.log(data));
+
 module.exports = router;
 
 
