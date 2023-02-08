f42bfc9a7fade0cb38f005d36d5bd3b83de929c4
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-08 12:47:42 -0800

    Add User input validation on user login backend endpoint

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -1,10 +1,9 @@
 // backend/routes/api/users.js
 const express = require('express')
 const router = express.Router();
+
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
-
-
 
 // Sign up
 router.post(
