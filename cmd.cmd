193aa3f2edfa3cd34355dd148efb9ade00d59f47
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-08 17:34:59 -0800

    update
    update

--- cmd.cmd
+++ cmd.cmd
@@ -11,3 +11,9 @@ git push origin main
 
 
 http://localhost:8000/api/csrf/restore
+
+
+    "unmigrate": "npx dotenv sequelize-cli db:migrate:undo:all",
+    "migrate": "npx dotenv sequelize-cli db:migrate",
+    "seed": "npx dotenv sequelize-cli db:seed:all",
+    "unseed": "npx dotenv sequelize-cli db:seed:undo:all"
