9b4b1c51ff5b639c66b79d71664f761d55c44707
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-08 16:04:07 -0800

    updated toSafeObject

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -7,8 +7,8 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     
     toSafeObject() {
-      const { id, firstName, lastName, username, email } = this; // context will be the User instance
-      return { id, firstName, lastName, username, email };
+      const { id, username, email } = this; // context will be the User instance
+      return { id, username, email };
     }
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
