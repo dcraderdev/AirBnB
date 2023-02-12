8332b66947a35864f88258b4a21dab839bdd407f
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-12 12:16:00 -0800

    changed booking seed

--- backend/db/seeders/20230212192815-base-spots.js
+++ backend/db/seeders/20230212192815-base-spots.js
@@ -1,61 +1,64 @@
-'use strict';
+// 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
 
 
-let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
-}
+// let options = {};
+// if (process.env.NODE_ENV === 'production') {
+//   options.schema = process.env.SCHEMA;  // define your schema in options object
+// }
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    options.tableName = 'Spots';
-    return queryInterface.bulkInsert(options, [
-      {
-        ownerId: 1,
-        address: 'Demo-road',
-        city: 'Demo-town',
-        state: 'Demo-fornia',
-        country: 'Demo-states',
-        lat: 8675309,
-        lng: 8675309,
-        name: 'Demo-cottage',
-        description: 'Nice lil Demo-rental',
-        price: 350,
-      },
-      {
-        ownerId: 1,
-        address: 'Demo-road',
-        city: 'Demo-town',
-        state: 'Demo-fornia',
-        country: 'Demo-states',
-        lat: 8675310,
-        lng: 8675311,
-        name: 'Demo-cottage 2',
-        description: 'Nice lil Demo-rental',
-        price: 360,
-      },
-      {
-        ownerId: 1,
-        address: 'Demo-road',
-        city: 'Demo-town',
-        state: 'Demo-fornia',
-        country: 'Demo-states',
-        lat: 8675312,
-        lng: 8675313,
-        name: 'Demo-cottage 3',
-        description: 'Nice lil Demo-rental',
-        price: 370,
-      },
-    ], {});
-  },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = 'Spots';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      ownerId: { [Op.in]: ['1'] }
-    }, {});
-  }
-};
+
+
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     options.tableName = 'Spots';
+//     return queryInterface.bulkInsert(options, [
+//       {
+//         ownerId: 1,
+//         address: 'Demo-road',
+//         city: 'Demo-town',
+//         state: 'Demo-fornia',
+//         country: 'Demo-states',
+//         lat: 8675309,
+//         lng: 8675309,
+//         name: 'Demo-cottage',
+//         description: 'Nice lil Demo-rental',
+//         price: 350,
+//       },
+//       {
+//         ownerId: 1,
+//         address: 'Demo-road',
+//         city: 'Demo-town',
+//         state: 'Demo-fornia',
+//         country: 'Demo-states',
+//         lat: 8675310,
+//         lng: 8675311,
+//         name: 'Demo-cottage 2',
+//         description: 'Nice lil Demo-rental',
+//         price: 360,
+//       },
+//       {
+//         ownerId: 1,
+//         address: 'Demo-road',
+//         city: 'Demo-town',
+//         state: 'Demo-fornia',
+//         country: 'Demo-states',
+//         lat: 8675312,
+//         lng: 8675313,
+//         name: 'Demo-cottage 3',
+//         description: 'Nice lil Demo-rental',
+//         price: 370,
+//       },
+//     ], {});
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     options.tableName = 'Spots';
+//     const Op = Sequelize.Op;
+//     return queryInterface.bulkDelete(options, {
+//       ownerId: { [Op.in]: ['1'] }
+//     }, {});
+//   }
+// };
