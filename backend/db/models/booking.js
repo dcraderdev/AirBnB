b7872c80f602a042520a8d9a5781b6fb66bddd33
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-12 11:16:52 -0800

    change to refs

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Booking.hasMany(models.Spot,{ foreignKey: 'spotId' })
-      Booking.hasMany(models.User,{ foreignKey: 'userId' })
+      Booking.hasOne(models.Spot,{ foreignKey: 'spotId' })
+      Booking.hasOne(models.User,{ foreignKey: 'userId' })
 
     }
   }
