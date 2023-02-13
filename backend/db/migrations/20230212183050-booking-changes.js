095281cfe4cc41236fa6f5b04e4f69a22d43655f
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-13 14:30:40 -0800

    here goes nothing2

--- backend/db/migrations/20230212183050-booking-changes.js
+++ backend/db/migrations/20230212183050-booking-changes.js
@@ -5,29 +5,29 @@ module.exports = {
   async up (queryInterface, Sequelize) {
 
 
-    await queryInterface.addColumn('Bookings','spotId', { 
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references:{
-        model: 'Spots'
-      },
-      onDelete: 'cascade'
-    });
+    // await queryInterface.addColumn('Bookings','spotId', { 
+    //   type: Sequelize.INTEGER,
+    //   allowNull: false,
+    //   references:{
+    //     model: 'Spots'
+    //   },
+    //   onDelete: 'cascade'
+    // });
 
-    await queryInterface.addColumn('Bookings','userId', { 
-      type: Sequelize.INTEGER,
-      allowNull: true,
-      references:{
-        model: 'Users',
-      } 
-    });
+    // await queryInterface.addColumn('Bookings','userId', { 
+    //   type: Sequelize.INTEGER,
+    //   allowNull: false,
+    //   references:{
+    //     model: 'Users',
+    //   } 
+    // });
 
  
 
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Bookings', 'spotId');
-    await queryInterface.removeColumn('Bookings', 'userId');
+    // await queryInterface.removeColumn('Bookings', 'spotId');
+    // await queryInterface.removeColumn('Bookings', 'userId');
   }
 };
