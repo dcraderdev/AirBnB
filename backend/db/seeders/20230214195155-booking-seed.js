959d77318c07cbebf0f56742ef3f291705044fa5
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-14 12:01:17 -0800

    change

---
+++ backend/db/seeders/20230214195155-booking-seed.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+const { QueryInterface } = require('sequelize');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'Bookings';
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          spotId: 1,
+          userId: 2,
+          startDate: new Date('2023-02-10'),
+          endDate: new Date('2023-02-12'),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete(options, {}, {});
+  },
+};
