0310acf41e8bcab4d5cd82d6514bdc3ab7522a64
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-14 11:39:30 -0800

    change

--- backend/db/seeders/20230212193841-base-bookings.js
+++ backend/db/seeders/20230212193841-base-bookings.js
@@ -11,36 +11,36 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
 
-    options.tableName = 'Bookings';
-    return queryInterface.bulkInsert(options, [
-      {
-        spotId: 1,
-        userId: 2,
-        startDate: '2023-02-10',
-        endDate: '2023-02-12'
-      },
-      {
-        spotId: 1,
-        userId: 2,
-        startDate: '2023-03-10',
-        endDate: '2023-03-12'
-      },
-      {
-        spotId: 1,
-        userId: 2,
-        startDate: '2023-04-10',
-        endDate: '2023-04-12'
-      },
-    ], {});
+    // options.tableName = 'Bookings';
+    // return queryInterface.bulkInsert(options, [
+    //   {
+    //     spotId: 1,
+    //     userId: 2,
+    //     startDate: '2023-02-10',
+    //     endDate: '2023-02-12'
+    //   },
+    //   {
+    //     spotId: 1,
+    //     userId: 2,
+    //     startDate: '2023-03-10',
+    //     endDate: '2023-03-12'
+    //   },
+    //   {
+    //     spotId: 1,
+    //     userId: 2,
+    //     startDate: '2023-04-10',
+    //     endDate: '2023-04-12'
+    //   },
+    // ], {});
   },
 
   async down (queryInterface, Sequelize) {
 
-    options.tableName = 'Bookings';
-    const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: ['1'] }
-    }, {});
+    // options.tableName = 'Bookings';
+    // const Op = Sequelize.Op;
+    // return queryInterface.bulkDelete(options, {
+    //   spotId: { [Op.in]: ['1'] }
+    // }, {});
  
   }
 };
