a63bd939e9b2ac44f96450d1175e5e41d29d9502
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-14 13:37:50 -0800

    working version

--- backend/db/seeders/20230214195155-booking-seed.js
+++ backend/db/seeders/20230214195155-booking-seed.js
@@ -10,19 +10,19 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    options.tableName = 'Bookings';
-    return queryInterface.bulkInsert(options,[
-        {
-          spotId: 1,
-          userId: 2,
-          startDate: new Date('2023-02-10'),
-          endDate: new Date('2023-02-12'),
-        },
-      ],{}
-    );
+    // options.tableName = 'Bookings';
+    // return queryInterface.bulkInsert(options,[
+    //     {
+    //       spotId: 1,
+    //       userId: 2,
+    //       startDate: new Date('2023-02-10'),
+    //       endDate: new Date('2023-02-12'),
+    //     },
+    //   ],{}
+    // );
   },
 
   async down(queryInterface, Sequelize) {
-    return queryInterface.bulkDelete(options, {}, {});
+    // return queryInterface.bulkDelete(options, {}, {});
   },
 };
