c8e3b2ca124913c8aaac0ce1d22757eb683572b5
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-20 16:50:38 -0800

    round
    did

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -155,7 +155,10 @@ router.get('/:spotId', requireAuth, async (req, res, next) => {
         'createdAt', 
         'updatedAt',
         [Sequelize.fn('COUNT', Sequelize.col('Reviews.id'),), 'numReviews'],
-        [Sequelize.fn('AVG', Sequelize.col('Reviews.stars'),), 'avgStarRating'],
+        [Sequelize.fn('ROUND', Sequelize.fn('AVG', Sequelize.col('Reviews.stars')), 1), 'avgStarRating'],
+
+
+
       ],
       include: [
         {
