82743adc7ef844dd33f2e819d6642fd2b4e415d9
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-22 15:49:22 -0800

    rounded avgStar

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -147,7 +147,7 @@ router.get('/:spotId', requireAuth, async (req, res, next) => {
         'createdAt',
         'updatedAt',
         [Sequelize.fn('COUNT', Sequelize.col('Reviews.id'),), 'numReviews'],
-        [Sequelize.fn('AVG', Sequelize.col('Reviews.stars'),), 'avgStarRating'],
+        [Sequelize.fn('ROUND', Sequelize.fn('AVG', Sequelize.col('Reviews.stars')),1), 'avgStarRating'],
     ],
     include: [
         {
