17a5e80d8cc09caa1eb081590008d1aba311f33e
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-27 13:26:18 -0800

    attempt
    2

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -77,7 +77,7 @@ router.get('/current', requireAuth, async (req, res, next) => {
   
       
       // const previewImage = Spot.SpotImages.length > 0 ? Spot.SpotImages[0].url : null;
-        const previewImage = Spot.SpotImages.filter(image => image.preview)[0]?.url || null;
+        const previewImage = Spot.SpotImages.filter(image => image.preview).url || null;
       return {
         id,
         spotId,
