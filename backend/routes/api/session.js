e353d3f5dac5bbb7f7c35019672791d1c5d323a0
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-26 22:12:45 -0800

    fixed token

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -23,7 +23,7 @@ router.post('/',validateLogin, async (req, res, next) => {
               lastName: user.lastName,
               email: user.email,
               username: user.username,
-              token: token,
+              // token: token,
             });
   }
 
