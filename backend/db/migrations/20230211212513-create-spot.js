581bc5face6d41b89083733638ff4023c2841707
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-02-26 20:09:25 -0800

    fixed decimal in migration

--- backend/db/migrations/20230211212513-create-spot.js
+++ backend/db/migrations/20230211212513-create-spot.js
@@ -58,7 +58,7 @@ module.exports = {
         allowNull: false
       },
       price: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.DECIMAL,
         allowNull: false
       },
       createdAt: {
