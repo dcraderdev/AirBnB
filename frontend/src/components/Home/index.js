b2d0b24ecb8a9f375e3a0ca3605f7796d5870965
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-03-28 17:00:39 -0700

    fasjgdhfuilasgldfvcjhbasdfv
    rip dude just rip

---
+++ frontend/src/components/Home/index.js
@@ -0,0 +1,21 @@
+import React, { useEffect,useRef, useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect, useHistory } from 'react-router-dom';
+import './Home.css'
+import Spots from '../Spots';
+
+
+const Home = ({isLoaded}) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+
+  return (
+    <div>
+      <Spots/>
+    </div>
+  )
+}
+
+export default Home
