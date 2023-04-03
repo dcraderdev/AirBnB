54dcc38471b7089cffcb0bf1dff5d7e03940870f
dcraderdev <90993510+dcraderdev@users.noreply.github.com>
2023-04-02 19:57:34 -0700

    tooltip lookin good

--- frontend/src/components/SpotTile/index.js
+++ frontend/src/components/SpotTile/index.js
@@ -1,53 +1,104 @@
+//SpotTile
 import React, { useState, useRef, useEffect } from 'react';
 
-import './SpotTile.css'
+import './SpotTile.css';
 import { Link } from 'react-router-dom';
 
-
-
 const SpotTile = ({ spot, setFavorites }) => {
-  let rating
-  
-  const { previewImage, city, state, country, lat, lng, price, avgRating, description } = spot;
 
-  const [favorite, setFavorite] = useState([false])
+  const [tooltip, setTooltip] = useState({ display: false, x: 0, y: 0 });
+  const [tooltipDisplay, setTooltipDisplay] = useState(false);
+  const [rating, setRating] = useState('');
+  const [tileDescription, setTileDescription] = useState('');
+  const [favorite, setFavorite] = useState([false]);
 
-  if(!avgRating) rating = 5
-  if(avgRating) rating = avgRating
-  let long = description + ' ' + description 
-  let shortDescription = long.slice(0,80) + '...'
+  const {
+    name,
+    previewImage,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    price,
+    avgRating,
+    description,
+  } = spot;
 
 
-  let heartFill = favorite ? "fa-regular fa-heart" : "fa-solid fa-heart" 
 
-const setFav = () =>{
-  setFavorite(!favorite)
-}
+  useEffect(()=>{
+    if(avgRating === null)setRating('new');
+    if (avgRating)setRating(avgRating)
+    setTileDescription((description + ' ' + description).slice(0, 80) + '...')  
+  },[])
+
+
+
+
+  let heartFill = favorite ? 'fa-regular fa-heart' : 'fa-solid fa-heart';
+
+  const setFav = () => {
+    setFavorite(!favorite);
+  };
+
+
+  const handleMouseMove = (e) => {
+    setTooltipDisplay(true)
+    setTooltip({ x: e.clientX, y: e.clientY + window.scrollY });
+  };
+
+  const handleMouseLeave = () => {
+    setTooltipDisplay(false);
+  };
+
 
   return (
-    
-    <Link key={spot.id} to={`/spots/${spot.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-        <div className="spot-tile">
-    <div className="imageContainer">
-      <img src={previewImage} alt="Spot preview" className="spot-image" />
-      <div className="heart-icon" onClick={setFav} >
+    <Link
+      key={spot.id}
+      to={`/spots/${spot.id}`}
+      style={{ textDecoration: 'none', color: 'inherit' }}
+    >
+      <div className="spot-tile" onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+        <div className="spot-tile-image-container">
+          <img src={previewImage} alt="Spot preview" className="spot-image" />
+          <div className="heart-icon" onClick={setFav}>
+            <div className={heartFill}></div>
+          </div>
+        </div>
 
-        <div className={heartFill} ></div>
-      </div>
-    </div>
+        <div className="spot-info">
+          <div className="location-rating">
+            <div className="location">
+              {city}, {state}
+            </div>
+            <div className="avg-rating">{rating} ⭐️</div>
+          </div>
+          <div className="price">${price} / night</div>
+        </div>
+        <div className="spot-description-line"></div>
+        <p className="spot-short-description">{tileDescription}</p>
 
-    <div className="spot-info">
-  <div className="location-rating">
-    <div className="location">{city}, {state}</div>
-    <div className="avg-rating">{rating} ⭐️</div>
-  </div>
-  <div className="price">${price} / night</div>
-</div>
-  <div className="spot-description-line"></div>
-  <p className="spot-short-description">{shortDescription}</p>
+      {tooltipDisplay && (
+        <div className="tooltip tooltiptext"
+        style={{
+          display: tooltipDisplay ? 'block' : 'none',
+          top: tooltip.y + 'px',
+          left: tooltip.x + 'px',
+        }}>
+          {name}
+          </div>
+      )}
+
       </div>
+
+
+
     </Link>
   );
 };
 
-export default SpotTile; 
+export default SpotTile;
+
+
+        // style={{ top: tooltip.y + 'px', left: tooltip.x + 'px' }}
--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -1,3 +1,4 @@
+//Spots
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import SpotTile from '../SpotTile';
